f87b1f87b2d670a51f332905bab03522f4f888df
Ann Mary Shaju <shajuannmary01@gmail.com>
2023-04-20 13:10:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name = "Loan Status Prediction",
+    name = "Home Loan Status Prediction",
     version = '0.0.1',
     author = "Ann Mary Shaju",
     author_email = "shajuannmary01@gmail.com",
